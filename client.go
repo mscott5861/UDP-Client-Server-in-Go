b52d1fd26fb88ea41e432cc52a8eda7f31dd965b
M. Adams <mscott5861@gmail.com>
2017-12-16 15:20:36 -0500

    Final post-testing implementation of client/server

---
+++ client.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "net"
+    "time"
+    "encoding/binary"
+    "hash/adler32"
+    "bytes"
+)
+
+func ParseErrorResponse(err error) {
+    if err != nil {
+        fmt.Println("Error: ", err)
+    }
+}
+
+// We take an int large enough to accommodate the largest we'll pass (uint64),
+// then allocate an appropriate sized byte array, typecast the argument, and
+// return.
+func GetIntBigEndianBytes(numBytes int, arg uint64) (buf []byte) {
+    buf = make([]byte, numBytes)
+    if numBytes == 2 {
+        binary.BigEndian.PutUint16(buf, uint16(arg))
+    } else if numBytes == 4 {
+        binary.BigEndian.PutUint32(buf, uint32(arg))
+    } else if numBytes == 8 {
+        binary.BigEndian.PutUint64(buf, uint64(arg))
+    }
+    return
+}
+
+func GetStrBigEndianBytes(str string) (buf []byte) {
+    msg := []byte(str)
+    msgBuf := new(bytes.Buffer)
+    err := binary.Write(msgBuf, binary.BigEndian, msg)
+    ParseErrorResponse(err)
+    buf = msgBuf.Bytes()
+    return
+}
+
+// Take the IP address as a net.IP object, create a byte array buffer,
+// convert the net.IP object to a 4-byte representation, then return
+// a Big Endian representation of the address
+
+func GetIPBigEndianBytes(ipAddr net.IP) (buf []byte) {
+    buf = make([]byte, 4)
+    ip := binary.BigEndian.Uint32(net.IP.To4(ipAddr))
+    binary.BigEndian.PutUint32(buf, ip)
+    return
+}
+
+func GetOutboundIPAddress() (net.IP) {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    ParseErrorResponse(err)
+    defer conn.Close()
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+    return localAddr.IP
+}
+
+
+func main() {
+    // First, let's gather our own local IP Address. The address returned by InterfaceAddrs
+    // at index 0 will always be the loopback device. We're looking for the IP on the LAN,
+    // at index 1.
+    myIPCIDR, err := net.InterfaceAddrs()
+    ParseErrorResponse(err)
+    myIP,_,err := net.ParseCIDR(myIPCIDR[1].String())
+    ParseErrorResponse(err)
+
+    // Get the server IP address passed as the first argument to the program
+    serverIP := os.Args[2]
+    // Get the client port passed as the second argument, then concatenate it
+    // to the loopback address
+    clientPort := os.Args[1]
+    clientIP := "127.0.0.1:0" + clientPort
+
+    ServerAddr, err := net.ResolveUDPAddr("udp", serverIP)
+    ParseErrorResponse(err)
+
+    LocalAddr, err := net.ResolveUDPAddr("udp", clientIP)
+    ParseErrorResponse(err)
+
+    Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+    ParseErrorResponse(err)
+
+    defer Conn.Close()
+    for {
+        // Handle creating our message
+        msg := "\"It always seems impossible until it's done.\" - Nelson Mandella"
+        msgBuf := GetStrBigEndianBytes(msg)
+
+        // Handle pushing our port
+        portBuf := GetIntBigEndianBytes(2, 21983)
+        concat1 := append(portBuf[:], msgBuf[:]...)
+
+        // Handle pushing our IP Address. Feed it a String copy of the IP address we
+        // gathered earlier (our IP on the LAN).
+        ipBuf := GetIPBigEndianBytes(net.ParseIP(myIP.String()))
+        concat2 := append(ipBuf[:], concat1[:]...)
+
+        // Handle creating our timestamp. Not much to see here.
+        timeBuf := GetIntBigEndianBytes(8, uint64(time.Now().Unix()))
+        concat3 := append(timeBuf[:], concat2[:]...)
+
+        // Handle creating our Adler32 checksum. It is passed the current payload,
+        // which it calculates a hash against. The value it returns is converted 
+        // into a Big Endian byte array, and then concatenated with the payload. 
+        checksumBuf := GetIntBigEndianBytes(4, uint64(adler32.Checksum(concat3)))
+        concat4 := append(checksumBuf[:], concat3[:]...)
+
+        _, err2 := Conn.Write(concat4)
+        ParseErrorResponse(err2)
+
+        // Send our packet every two seconds
+        time.Sleep(time.Second * 2)
+    }
+}
+
---
+++ server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os"
+    "encoding/binary"
+    "time"
+)
+
+func ParseErrorResponse(err error) {
+    if err != nil {
+        fmt.Println("Error: ", err)
+        os.Exit(0)
+    }
+}
+
+func main() {
+    ServerPort := ":" + os.Args[1]
+    ServerAddr, err := net.ResolveUDPAddr("udp", ServerPort)
+    ParseErrorResponse(err)
+
+    ServerConn, err := net.ListenUDP("udp", ServerAddr)
+    ParseErrorResponse(err)
+    defer ServerConn.Close()
+
+    // We'll allocate enough for the default UDP MTU limit, though
+    // we won't need it.
+    buf := make([]byte, 65535)
+    timesMsgReceived := 0
+
+    for {
+        n, _, err := ServerConn.ReadFromUDP(buf)
+        timesMsgReceived++
+        // Our message occupies bytes 18 -- length of message in the payload
+        fmt.Println("Received: ", string(buf[18:n]))
+
+        // Handle parsing our timestamp. An 8-byte Unix timestamp was sent at
+        // bytes 4-12
+        timestamp := int64(binary.BigEndian.Uint64(buf[4:12]))
+        clientTimestamp := time.Unix(timestamp, 0)
+
+        // Now get the server's own Unix timestamp
+        serverTimestamp := time.Unix(time.Now().Unix(), 0)
+        differenceInTime := serverTimestamp.Sub(clientTimestamp)
+        fmt.Println("Time elapsed between tx/rx: ", differenceInTime)
+
+        // Get the IP address stored at bytes 12-16
+        ip := binary.BigEndian.Uint32(buf[12:16])
+        ipB := make(net.IP, 4)
+        binary.BigEndian.PutUint32(ipB, ip)
+        fmt.Println("IP: ", ipB)
+        fmt.Println("Times message received: ", timesMsgReceived)
+        fmt.Println("\n")
+
+        if err != nil {
+            fmt.Println("Error: ", err)
+        }
+    }
+}
+
+
+
+
